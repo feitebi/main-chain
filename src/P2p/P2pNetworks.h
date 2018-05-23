c7000153b4964322ea79b744f4d6863ab4f29fff
dagdeng <mangoo.deng@gmail.com>
2018-05-24 04:18:47 +0800

    p2p interface
    p2p interface

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 04f8, 07a0, 0cdc, 9b75, 8581, 5286, 8766, 3eb9, 1c8f, 4ef5, 6a2d, 933f, 2a39, 0b9c, 3bab, 0x54} };
+}
+0x50
